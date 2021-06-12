a54d27d815e8ef37d214b176df7598ce97325136
dakshBatra <78405740+dakshBatra@users.noreply.github.com>
2021-06-12 13:54:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function preload() {
+    //load the images here
+    bgimg=loadImage("images/garden.png")
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+
+    background(bgimg);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
